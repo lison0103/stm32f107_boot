3016f177c0ae8c2545a793a604e558b462dfaafb
lisonchen <lison.chen@tkeap.com>
2016-06-24 16:52:40 +0800

    【GEC-CB-A】BSP: use TIM timer implement delay function

--- HARDWARE/TIMER/timer.h
+++ HARDWARE/TIMER/timer.h
@@ -21,7 +21,7 @@
 void TIM4_Int_Init(u16 arr,u16 psc);
 void TIM3_Int_Init(u16 arr,u16 psc);
 void TIM2_Int_Init(u16 arr,u16 psc);
-
+void TIM1_Int_Init(u16 arr,u16 psc);
 
 #endif  /* __TIMER_H */
 
